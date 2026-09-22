e81c5f61f42800e4c1670dd39af32762bd569350
Stev58 <127752234+Stev58@users.noreply.github.com>
2026-09-22 09:32:54 -0500

    added *

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Calculator")
+print("Multiplication Calculator")
+
+val1= int(input("Value 1: "))
+val2= int(input("Value 2: "))
+
+result = val1 * val2
+
+print(str(result) + " is the final value")
